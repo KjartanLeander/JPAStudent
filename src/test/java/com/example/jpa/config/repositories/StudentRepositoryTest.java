05590600e9b02297b0ea5f85bba6389d02574ab5
kjartanleander <Kjar0321@stud.kea.dk>
2023-09-07 14:16:35 +0200

    For at få action til at virke1

--- src/test/java/com/example/jpa/config/repositories/StudentRepositoryTest.java
+++ src/test/java/com/example/jpa/config/repositories/StudentRepositoryTest.java
@@ -3,6 +3,7 @@ package com.example.jpa.config.repositories;
 import com.example.jpa.model.Student;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDate;
@@ -11,7 +12,7 @@ import org.junit.Assert;
 
 
 import static org.junit.Assert.assertEquals;
-@SpringBootTest
+@DataJpaTest
 
 
 class StudentRepositoryTest {
