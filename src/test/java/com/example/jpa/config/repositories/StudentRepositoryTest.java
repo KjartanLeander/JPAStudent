1c47c8f82a88e3ad30bcde1b7e964c281ef75a37
kjartanleander <Kjar0321@stud.kea.dk>
2023-09-07 14:23:07 +0200

    ny test fra chat

--- src/test/java/com/example/jpa/config/repositories/StudentRepositoryTest.java
+++ src/test/java/com/example/jpa/config/repositories/StudentRepositoryTest.java
@@ -1,46 +1,43 @@
 package com.example.jpa.config.repositories;
 
 import com.example.jpa.model.Student;
+import com.example.jpa.config.repositories.StudentRepository;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDate;
 import java.util.List;
-import org.junit.Assert;
-
 
 import static org.junit.Assert.assertEquals;
-@DataJpaTest
 
-
+@SpringBootTest
 class StudentRepositoryTest {
-@Autowired StudentRepository studentRepository;
+    @Autowired
+    private StudentRepository studentRepository;
 
-
-
-@BeforeEach
-void setup(){
-    Student std = new Student();
-    std.setName("Tim");
-    studentRepository.save(std);
-}
-
-@Test
-    void testOneTim(){
-    List<Student>lst=studentRepository.findAllByName("Tim");
-    assertEquals(1,lst.size());
+    @BeforeEach
+    void setup() {
+        Student std = new Student();
+        std.setName("Tim");
+        studentRepository.save(std);
     }
 
     @Test
-    void testOneViggo(){
+    void testOneTim() {
+        List<Student> lst = studentRepository.findAllByName("Tim");
+        assertEquals(1, lst.size());
+    }
+
+    @Test
+    void testOneViggo() {
         Student s1 = new Student();
         s1.setName("Viggo");
-        s1.setBorndate(LocalDate.of(2010,11,12));
+        s1.setBorndate(LocalDate.of(2010, 11, 12));
         studentRepository.save(s1);
         List<Student> lst = studentRepository.findAllByName("Viggo");
-        assertEquals(1,lst.size());
-}
+        assertEquals(1, lst.size());
+    }
 }
