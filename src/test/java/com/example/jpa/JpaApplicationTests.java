126c9b84fa6e3f0d64cd2a2b9a4c4cac68153a20
kjartanleander <Kjar0321@stud.kea.dk>
2023-09-14 14:38:50 +0200

    ny

--- src/test/java/com/example/jpa/JpaApplicationTests.java
+++ src/test/java/com/example/jpa/JpaApplicationTests.java
@@ -1,9 +1,10 @@
 package com.example.jpa;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@DataJpaTest
 class JpaApplicationTests {
 
     @Test
